00632c104237b16a2edc695525a2a8955c205250
ibrahim <ibrahimemara@gmail.com>
2018-02-15 17:51:33 +0200

    learn

--- PycharmProjects/untitled1/main.py
+++ PycharmProjects/untitled1/main.py
@@ -1,3 +1,28 @@
+class Enemy:
+    life=3
+
+    def attack(self):
+        print('ouche')
+        self.life-=1
+    def checklife(self):
+        if self.life<=0:
+            print('a am dead')
+        else:
+            print(str(self.life)+"life left")
+
+enemy1=Enemy()
+enemy2=Enemy()
+
+enemy1.attack()
+enemy1.attack()
+
+enemy1.checklife()
+enemy2.checklife()
+
+
+
+"""
+
 while True:
     try:
         number=int(input("what is your age \n"))
@@ -11,10 +36,6 @@ while True:
         break
     finally:
         print("loop compltee ")
-
-
-
-"""
 
 import tuna
 import random
