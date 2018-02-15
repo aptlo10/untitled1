8e4f7468c92db7159691332b4f0e54168be16ed9
ibrahim <ibrahimemara@gmail.com>
2018-02-15 15:38:04 +0200

    learn

--- PycharmProjects/untitled1/a.py
+++ PycharmProjects/untitled1/a.py
@@ -1,5 +1,29 @@
 
 
+def health(age,apple_etae,cig):
+    answer=(100-age)+(apple_etae*3.5)+(cig *2)
+    print(answer)
+
+ibrahim=[27,5,0]
+
+health(ibrahim[0],ibrahim[1],ibrahim[2])
+health(*ibrahim)
+
+
+"""
+
+def add_number(*tuna):
+    total=0
+    for a in tuna:
+        total+=a
+    print(total)
+add_number(3)
+add_number(3,5,7)
+add_number(1,3,70)
+
+
+
+
 def corn():
     a=100
     print(a)
@@ -8,6 +32,8 @@ def pop():
     print(a)
 
 corn()
-"""pop()"""
+pop()
 
-""" heloo ahmed """
+ heloo ahmed
+
+"""
