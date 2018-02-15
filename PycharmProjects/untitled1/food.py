da2739f821c577527c4b6bbaaef3c068b4fe3a2d
ibrahim <ibrahimemara@gmail.com>
2018-02-15 14:21:51 +0200

    learn

---
+++ PycharmProjects/untitled1/food.py
@@ -0,0 +1,21 @@
+
+food =["meat","tuna","fish","meat"]
+
+"""
+for x in range(10,20,3):
+        print(x)
+
+"""
+'''
+x=5
+while x<=10:
+    print(x)
+    x +=1
+
+'''
+
+
+x=20
+while x>=10:
+    print(x)
+    x -=1
---
+++ PycharmProjects/untitled1/new.py
@@ -0,0 +1,12 @@
+def  x(sex="nothing"):
+    if sex is 'm':
+        sex="male"
+    elif sex is'f':
+        sex="female"
+    print(sex)
+
+
+x('m')
+x('f')
+x('f')
+x()
