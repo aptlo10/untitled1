fe74012cf7601d03eddc94f45041d37d2236625f
ibrahim <ibrahimemara@gmail.com>
2018-02-20 13:56:34 +0200

    learn

--- PycharmProjects/untitled1/learn--3/1.py
+++ PycharmProjects/untitled1/learn--3/1.py
@@ -1,10 +1,30 @@
+"""not
+and
+or
+"""
+
+a= True
+b=False
+
+print("ali"!="pop")
+
+
+print ('{:<20}'.format("text"))
+print('{:>20}'.format("text"))
+print ('{:<20}'.format("text"))
+
+
+#print(len(string))
+"""
 string="hello world ibrahim "
 
 d= "heloo world ibrahim "
 
 
-#print(len(string))
-"""print(d[-2])
+print ('{:<20}'.format("text"))
+print('{:>20}'.format("text"))
+
+print(d[-2])
 print(string[3:7])
 string2='concat'+'cat'+'dog'
 print(string2)
@@ -12,8 +32,7 @@ print(string2)
 print("today is a good day i have a  {0} cups of {1}".format(3,"coffee"))
 print("value of my pricess is :{x},{y},{z}".format(x=2.0,y=1.5,z=5))
 """
-print ('{:<20}'.format("text"))
-print('{:>20}'.format("text"))
+
 
 """
  clear screen
---
+++ PycharmProjects/untitled1/learn--3/2.py
@@ -0,0 +1,30 @@
+"""for i in range(1,11):
+    print('{:<3}|'.format(i),end="")
+"""
+a=10
+b=21
+print ('{:>15}'.format("root"))
+print(a,'||{:>20}|'.format(""),b)
+print('{:>15}|||'.format("text"))
+print ('{:<15}|'.format("text"))
+
+
+
+"""
+
+string ="heloo world"
+for i in range(len(string)):
+    print(string[i])
+
+
+for char in string:
+    print(char)
+
+for i in range(3):
+    for j in range(2):
+        print(j)
+
+passort=""
+me="hi" if passort=="helooo" or passort=="hi" else "hey"
+print(me)
+"""
