922f4c96915e29d912e8429ae7f82010cd644803
ibrahim <ibrahimemara@gmail.com>
2018-02-20 12:27:03 +0200

    learn

---
+++ PycharmProjects/untitled1/learn--3/1.py
@@ -0,0 +1,25 @@
+string="hello world ibrahim "
+
+d= "heloo world ibrahim "
+
+
+#print(len(string))
+"""print(d[-2])
+print(string[3:7])
+string2='concat'+'cat'+'dog'
+print(string2)
+
+print("today is a good day i have a  {0} cups of {1}".format(3,"coffee"))
+print("value of my pricess is :{x},{y},{z}".format(x=2.0,y=1.5,z=5))
+"""
+print ('{:<20}'.format("text"))
+print('{:>20}'.format("text"))
+
+"""
+ clear screen
+ import os
+
+clear = lambda:os.system('cls')
+
+"""
+#asdsa
